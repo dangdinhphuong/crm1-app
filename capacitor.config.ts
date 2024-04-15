d16e1363fd1d0768dc4e19afd3b3826522b782ee
phuongdd@educa.vn <phuongdd@educa.vn>
2024-04-15 14:15:47 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.crm1',
+  appName: 'crm1',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
